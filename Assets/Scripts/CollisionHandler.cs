0fc0551492954c3676c4d3fa843934076894ac34
SimoMarr <simonemarroncelli8@gmail.com>
2022-02-13 12:09:37 +0100

    Crash/Success sequences

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -3,14 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float levelLoadDelay = 1f;
     void OnCollisionEnter(Collision other)
     {
         switch(other.gameObject.tag)
         {
             case "Friendly": Debug.Log("Bumped into friendly object."); break;
-            case "Finish": NextLevel(); break;
-            default: ReloadLevel(); break;
+            case "Finish": SuccessSequence(); break;
+            default: CrashSequence(); break;
         }    
+    }
+
+    void SuccessSequence()
+    {
+        Invoke("NextLevel", levelLoadDelay);
+        GetComponent<Movement>().enabled = false;
+        GetComponent<AudioSource>().Stop();
+    }
+
+    void CrashSequence()
+    {
+        Invoke("ReloadLevel", levelLoadDelay);
+        GetComponent<Movement>().enabled = false;
+        GetComponent<AudioSource>().Stop();
     }
 
     void NextLevel()
